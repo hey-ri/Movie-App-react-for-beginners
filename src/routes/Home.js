7a02e1249a57a2f4ae834bbf88cf10460e87db5b
hye-ri <kwonhr01@naver.com>
2022-08-17 17:32:49 +0900

    stylecommit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../component/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -20,11 +21,13 @@ function Home() {
     }, []);
     console.log(movies);
     return (
-        <div>
+        <div className={styles.container}>
             {loading ? (
-                <h1>Loading...</h1>
+                <div className={styles.loader}>
+                    <h1>Loading...</h1>
+                </div>
             ) : (
-                <div>
+                <div className={styles.movies}>
                     {movies.map((movie) => (
                         <Movie
                             key={movie.id}
